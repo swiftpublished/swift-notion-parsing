08fb592ad8d0674d6d0c5e5cfb75050bb55c44da
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-02-19 09:48:45 +0530

    Initial Commit

---
+++ Tests/swift-notion-parsingTests/swift_notion_parsingTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_notion_parsing
+
+final class swift_notion_parsingTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
