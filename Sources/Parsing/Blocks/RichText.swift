71d2f1893f64b126aaa959bea19fe78fab845477
Kanagasabapathy <trksabapathy7@gmail.com>
2024-06-26 08:44:45 +0530

    Modifies the RichText model based on the block object

--- Sources/Parsing/Blocks/RichText.swift
+++ Sources/Parsing/Blocks/RichText.swift
@@ -3,9 +3,9 @@ import MacrosInterface
 
 public struct RichText: Equatable, Codable {
     public let type: Types
-    public let annotations: Annotations
+    public let annotations: Annotations?
 
-    public init(type: Types, annotations: Annotations) {
+    public init(type: Types, annotations: Annotations? = .none) {
         self.type = type
         self.annotations = annotations
     }
@@ -25,11 +25,11 @@ public extension RichText {
 
     @PublicInit
     struct Annotations: Codable, Equatable {
-        public let bold: Bool
-        public let italic: Bool
-        public let strikethrough: Bool
-        public let underline: Bool
-        let code: Bool
+        public let bold: Bool?
+        public let italic: Bool?
+        public let strikethrough: Bool?
+        public let underline: Bool?
+        let code: Bool?
     }
 }
 
@@ -54,7 +54,7 @@ extension RichText {
             self.type = .text(text)
         }
 
-        self.annotations = try container.decode(Annotations.self, forKey: .annotations)
+        self.annotations = try container.decodeIfPresent(Annotations.self, forKey: .annotations)
     }
 
     public func encode(to encoder: any Encoder) throws {
@@ -67,7 +67,7 @@ extension RichText {
             try typesContainer.encode(text, forKey: .text)
         }
 
-        try container.encode(annotations, forKey: .annotations)
+        try container.encodeIfPresent(annotations, forKey: .annotations)
     }
 }
 
