228fe93f2ddde7101c85187f01c3a52e65269797
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-07-22 11:06:08 +0530

    parses type of Video as html will be handled differently

--- Sources/Parsing/Blocks/Video.swift
+++ Sources/Parsing/Blocks/Video.swift
@@ -1,4 +1,3 @@
-import Foundation
 import MacrosInterface
 
 public extension Block {
@@ -6,9 +5,29 @@ public extension Block {
     @CodingKeys
     struct Video: Codable, Equatable {
         public let file: File
+        public let type: Types
+
+        public enum Types {
+            case youtube
+            case vimeo
+            case unknown
+        }
 
         public init(from decoder: Decoder) throws {
             self.file = try File(from: decoder)
+
+            if let host = file.type.url.host() {
+                switch host {
+                case let host where host.contains("youtube.com"):
+                    self.type = .youtube
+                case let host where host.contains("vimeo.com"):
+                    self.type = .vimeo
+                default:
+                    self.type = .unknown
+                }
+            } else {
+                self.type = .unknown
+            }
         }
 
         public func encode(to encoder: any Encoder) throws {
