72ed5c72e6c143b07ebf2142a12ab07924c1a525
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-02 21:43:19 +0530

    Adds Embed Block

---
+++ Sources/Parsing/Blocks/Embed.swift
@@ -0,0 +1,37 @@
+import MacrosInterface
+
+public extension Block {
+    @PublicInit
+    @CodingKeys
+    struct Embed: Codable, Equatable {
+        public let file: File
+        public let type: Types
+
+        public enum Types {
+            case youtube
+            case vimeo
+            case unknown
+        }
+
+        public init(from decoder: Decoder) throws {
+            self.file = try File(from: decoder)
+
+            if let host = file.type.url.host() {
+                switch host {
+                case let host where host.contains("youtube.com"):
+                    self.type = .youtube
+                case let host where host.contains("vimeo.com"):
+                    self.type = .vimeo
+                default:
+                    self.type = .unknown
+                }
+            } else {
+                self.type = .unknown
+            }
+        }
+
+        public func encode(to encoder: any Encoder) throws {
+            try file.encode(to: encoder)
+        }
+    }
+}
