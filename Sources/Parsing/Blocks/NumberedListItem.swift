7ab2f832bf5fa50c179f9cd3f580eeb649cc9257
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-02 22:15:41 +0530

    Adds NumberedListItem

--- Sources/Parsing/Blocks/NumberedListItem.swift
+++ Sources/Parsing/Blocks/NumberedListItem.swift
@@ -1,6 +1,7 @@
 import MacrosInterface
 
 public extension Block {
+    @PublicInit
     @CodingKeys(using: .snake_case)
     struct NumberedListItem: Codable, Equatable {
         @CodingKey(name: "rich_text")
