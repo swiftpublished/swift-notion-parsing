b351afc58108318f1396caebc572dc81f6221387
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-02 23:33:18 +0530

    Adds tests for Table Block

---
+++ Tests/ParsingTests/Blocks/TableTests.swift
@@ -0,0 +1,20 @@
+@testable import NotionParsing
+@testable import NotionParsingTestHelpers
+import XCTest
+
+final class TableTests: XCTestCase {
+    func test_basic_text() throws {
+        let json = """
+                {
+                    "type": "table",
+                    "table": {
+                        "table_width": 2,
+                        "has_column_header": true,
+                        "has_row_header": true
+                    }
+                }
+                """
+        let expected: Block.Table = .createSampleTableData(width: 2, hasColumnHeader: true, hasRowHeader: true)
+        try assert(decoding: json, to: expected)
+    }
+}
