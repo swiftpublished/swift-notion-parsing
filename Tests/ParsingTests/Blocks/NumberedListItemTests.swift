9088b752f1256834b030960e34f0495789c5790a
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-02 22:25:56 +0530

    Adds Tests for NumberedListItem Block

--- Tests/ParsingTests/Blocks/NumberedListItemTests.swift
+++ Tests/ParsingTests/Blocks/NumberedListItemTests.swift
@@ -1,8 +1,35 @@
-//
-//  File.swift
-//  
-//
-//  Created by kanagasabapathy on 02/08/24.
-//
+@testable import NotionParsing
+@testable import NotionParsingTestHelpers
+import XCTest
 
-import Foundation
+final class NumberedListItemTests: XCTestCase {
+    func test_basic_text() throws {
+        let json: String = """
+        {
+            "rich_text": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": "Number 1",
+                        "link": null
+                    },
+                    "annotations": {
+                        "bold": false,
+                        "italic": false,
+                        "strikethrough": false,
+                        "underline": false,
+                        "code": false,
+                        "color": "default"
+                    },
+                    "plain_text": "Number 1",
+                    "href": null
+                }
+            ]
+        }
+        """
+
+        let expected = Block.NumberedListItem(richTexts: [.text("Number 1")], children: nil)
+
+        try assert(decoding: json, to: expected)
+    }
+}
