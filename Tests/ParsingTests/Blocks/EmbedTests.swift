c1c2cb11f6e137f47c175ed91cc471855cac84ce
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-03 08:42:43 +0530

    Adds EmbedTests

---
+++ Tests/ParsingTests/Blocks/EmbedTests.swift
@@ -0,0 +1,39 @@
+@testable import NotionParsing
+@testable import NotionParsingTestHelpers
+import XCTest
+
+final class EmbedTests: XCTestCase {
+    func test_basic() throws {
+        let json: String = """
+            {
+                "caption": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": "ECR Beach, Chennai",
+                            "link": null
+                        },
+                        "annotations": {
+                            "bold": false,
+                            "italic": false,
+                            "strikethrough": false,
+                            "underline": false,
+                            "code": false,
+                            "color": "default"
+                        },
+                        "plain_text": "ECR Beach, Chennai",
+                        "href": null
+                    }
+                ],
+                "url": "https://maps.app.goo.gl/nZ1DJcEizc6ydmJKA"
+            }
+        """
+
+        let expected: Block.Embed = .embed(url: "https://maps.app.goo.gl/nZ1DJcEizc6ydmJKA", caption: [.text("ECR Beach, Chennai")])
+
+        try assert(decoding: json, to: expected)
+
+    }
+}
+
+
