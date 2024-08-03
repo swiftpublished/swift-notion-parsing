6f990e372dd9401a8eb4c122d6bd493b52ec910d
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-03 14:05:44 +0530

    Modified the Callout Block as per the requirement

--- Sources/Parsing/Blocks/Callout.swift
+++ Sources/Parsing/Blocks/Callout.swift
@@ -11,13 +11,15 @@ public extension Block {
         public let icon: Icon
         
         public init(from decoder: any Decoder) throws {
-            self.richTexts = try [RichText](from: decoder)
-            self.icon = try Icon(from: decoder)
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.richTexts = try container.decode([RichText].self, forKey: .richTexts)
+            self.icon = try container.decode(Icon.self, forKey: .icon)
         }
 
         public func encode(to encoder: any Encoder) throws {
-            try richTexts.encode(to: encoder)
-            try icon.encode(to: encoder)
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(richTexts, forKey: .richTexts)
+            try container.encode(icon, forKey: .icon)
         }
     }
 }
@@ -25,65 +27,52 @@ public extension Block {
 public extension Block {
     @PublicInit
     struct Icon: Codable, Equatable {
-        public let type: Types
+        public let type: IconType
 
-        public enum Types: Codable, Equatable {
+        public enum IconType: Codable, Equatable {
             case emoji(String)
             case external(External)
-            case notion(NotionHosted)
-            public var url: URL? {
-                switch self {
-                case .notion(let file):
-                    file.url
-                case .external(let file):
-                    file.url
-                case .emoji(_):
-                    nil
-                }
-            }
+            case file(NotionHosted)
         }
 
-        enum CodingKeys: CodingKey {
+        enum CodingKeys: String, CodingKey {
             case type
-
-            enum Types: String, CodingKey, Decodable {
-                case emoji
-                case file
-                case external
-            }
+            case emoji
+            case external
+            case file
         }
 
         public init(from decoder: any Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
 
-            let type = try container.decode(CodingKeys.Types.self, forKey: .type)
-            let typesContainer = try decoder.container(keyedBy: CodingKeys.Types.self)
+            let type = try container.decode(String.self, forKey: .type)
             switch type {
-            case .emoji:
-                let emoji = try typesContainer.decode(String.self, forKey: .emoji)
+            case "emoji":
+                let emoji = try container.decode(String.self, forKey: .emoji)
                 self.type = .emoji(emoji)
-            case .file:
-                let file = try typesContainer.decode(NotionHosted.self, forKey: .file)
-                self.type = .notion(file)
-            case .external:
-                let file = try typesContainer.decode(External.self, forKey: .external)
+            case "file":
+                let file = try container.decode(NotionHosted.self, forKey: .file)
+                self.type = .file(file)
+            case "external":
+                let file = try container.decode(External.self, forKey: .external)
                 self.type = .external(file)
+            default:
+                throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Unknown icon type")
             }
         }
 
         public func encode(to encoder: any Encoder) throws {
             var container = encoder.container(keyedBy: CodingKeys.self)
-            var typesContainer = encoder.container(keyedBy: CodingKeys.Types.self)
             switch self.type {
-            case .notion(let file):
-                try container.encode(CodingKeys.Types.file.rawValue, forKey: .type)
-                try typesContainer.encode(file, forKey: .file)
-            case .external(let file):
-                try container.encode(CodingKeys.Types.external.rawValue, forKey: .type)
-                try typesContainer.encode(file, forKey: .external)
-            case .emoji(let value):
-                try container.encode(CodingKeys.Types.emoji.rawValue, forKey: .type)
-                try typesContainer.encode(value, forKey: .external)
+            case .emoji(let emoji):
+                try container.encode("emoji", forKey: .type)
+                try container.encode(emoji, forKey: .emoji)
+            case .external(let external):
+                try container.encode("external", forKey: .type)
+                try container.encode(external, forKey: .external)
+            case .file(let file):
+                try container.encode("file", forKey: .type)
+                try container.encode(file, forKey: .file)
             }
         }
 
