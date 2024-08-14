f2e977946e229dd6d42bd52e466ca5ae4a27c662
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-14 10:55:46 +0530

    Updated MentionTests - Page Type

--- Tests/ParsingTests/Blocks/MentionTests.swift
+++ Tests/ParsingTests/Blocks/MentionTests.swift
@@ -62,4 +62,32 @@ final class MentionTests: XCTestCase {
         let expected: RichText = .mention( "2024-08-13", startDate: "2024-08-13")
         try assert(decoding: json, to: expected)
     }
+
+    func test_mention_page() throws {
+        let json: String = """
+        {
+            "type": "mention",
+            "mention": {
+                "type": "page",
+                "page": {
+                    "id": "cf5573de-e85d-41e3-aa75-e6ac1fbf15e1"
+                }
+            },
+            "annotations": {
+                "bold": false,
+                "italic": false,
+                "strikethrough": false,
+                "underline": false,
+                "code": false,
+                "color": "default"
+            },
+            "plain_text": "Untitled",
+            "href": "https://www.notion.so/cf5573dee85d41e3aa75e6ac1fbf15e1"
+        }
+        """
+
+
+        let expected: RichText = .mention("Untitled", pageID: "cf5573de-e85d-41e3-aa75-e6ac1fbf15e1", href: "https://www.notion.so/cf5573dee85d41e3aa75e6ac1fbf15e1")
+        try assert(decoding: json, to: expected)
+    }
 }
