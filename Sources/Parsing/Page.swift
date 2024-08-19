5e0564491e5b7e41a9783f6719441fc1c6e67cca
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-08-19 20:53:58 +0530

    Parses few extra properties

--- Sources/Parsing/Page.swift
+++ Sources/Parsing/Page.swift
@@ -6,6 +6,7 @@ import MacrosInterface
 public struct Page: Codable, Equatable {
     let id: UUID
     let lastEditedTime: Date
+    public let cover: Block.File?
     public let properties: Properties
     public var content: Content?
 
@@ -16,15 +17,37 @@ public struct Page: Codable, Equatable {
 
         @CodingKey(name: "Rich Title")
         public let richTitle: RichTitle?
+
+        @CodingKey(name: "Meta Title")
+        public let metaTitle: MetaTitle?
+
+        @CodingKey(name: "Meta Description")
+        public let metaDescription: MetaDescription?
+
         @PublicInit
         @CodingKeys
         public struct Title: Codable, Equatable {
             @CodingKey(name: "title")
             public let richTexts: [RichText]
         }
+
         @PublicInit
         @CodingKeys
         public struct RichTitle: Codable, Equatable {
+            @CodingKey(name: "rich_text")
+            public let richTexts: [RichText]
+        }
+
+        @PublicInit
+        @CodingKeys
+        public struct MetaTitle: Codable, Equatable {
+            @CodingKey(name: "rich_text")
+            public let richTexts: [RichText]
+        }
+
+        @PublicInit
+        @CodingKeys
+        public struct MetaDescription: Codable, Equatable {
             @CodingKey(name: "rich_text")
             public let richTexts: [RichText]
         }
