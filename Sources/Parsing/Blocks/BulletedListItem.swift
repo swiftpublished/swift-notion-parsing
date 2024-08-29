1b35929d6d5733872ac59d41309e96a3d2c6d7f7
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2024-08-29 09:37:07 +0100

    Make richTexts public in Block

--- Sources/Parsing/Blocks/BulletedListItem.swift
+++ Sources/Parsing/Blocks/BulletedListItem.swift
@@ -1,11 +1,12 @@
 import MacrosInterface
 
 public extension Block {
+    
     @CodingKeys(using: .snake_case)
     struct BulletedListItem: Codable, Equatable {
+        
         @CodingKey(name: "rich_text")
-        let richTexts: [RichText]
-
+        public let richTexts: [RichText]
         let children: [Block]?
     }
 }
