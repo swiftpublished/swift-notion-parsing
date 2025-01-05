c5c863ddbca88fb642054627b9610c9980d56496
Shahrukh Alam <alam.shahrukh786@gmail.com>
2025-01-05 15:37:37 +0530

    Logo instead of Image

--- Sources/Parsing/SponsorPage.swift
+++ Sources/Parsing/SponsorPage.swift
@@ -10,16 +10,16 @@ public struct SponsorPage: Codable, Equatable {
     @CodingKeys
     public struct Properties: Codable, Equatable {
         @CodingKey(name: "Title")
-        public let title: Title
+        public let title: Text
 
         @CodingKey(name: "Description")
         public let description: Text
 
-        @CodingKey(name: "Image")
-        public let image: Block.URL
+        @CodingKey(name: "Logo")
+        public let logo: Block.URL
 
-        @CodingKey(name: "Image Alternate Text")
-        public let imageAlternateText: Text
+        @CodingKey(name: "Logo Alternate Text")
+        public let logoAlternateText: Text
 
         @CodingKey(name: "CTA")
         public let cta: Text
@@ -29,13 +29,6 @@ public struct SponsorPage: Codable, Equatable {
 
         @CodingKey(name: "Status")
         public let status: Block.Checkbox
-
-        @PublicInit
-        @CodingKeys
-        public struct Title: Codable, Equatable {
-            @CodingKey(name: "title")
-            public let richTexts: [RichText]
-        }
 
         @PublicInit
         @CodingKeys
